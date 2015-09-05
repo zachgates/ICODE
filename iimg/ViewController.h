eb3b898e0d8494c162864734a65176778fee3d4c
Zach Gates <thezachgates@gmail.com>
2015-09-04 23:34:27 -0500

    initiated project

---
+++ iimg/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iimg
+//
+//  Created by Zach Gates on 9/4/15.
+//  Copyright (c) 2015 Zach Gates. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iimg/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iimg
+//
+//  Created by Zach Gates on 9/4/15.
+//  Copyright (c) 2015 Zach Gates. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
